6832171cb5e826b4b9a05547065c543d221408d7
Étienne Barrié <etienne.barrie@gmail.com>
2026-04-12 19:05:08 +0200

    target: show usage without argument or target to clear

--- .local/share/dotfiles/target.bash
+++ .local/share/dotfiles/target.bash
@@ -1,12 +1,10 @@
 target() {
-	[[ $# = 0 ]] && { unset make_target; return 0; }
-	if [[ $# -gt 1 || ! -d target/"$1" ]]; then
-		local targets=()
-		while read -r target; do
-			targets+=("${target#target/}")
-		done < <(find target -d -depth 1)
-		printf -v targets "%s|" "${targets[@]}"
-		>&2 echo "usage: target ${targets%?}"
+	[[ $# = 0 && -v make_target ]] && { unset make_target; return 0; }
+	if [[ $# -ne 1 || -z $1 || ! -d target/$1/ ]]; then
+		local targets choice
+		_comp_compgen -v targets -C target -- -d
+		printf -v choice "%s|" "${targets[@]#target/}"
+		>&2 echo "usage: target ${choice%|}"
 		return 1
 	fi
 	# shellcheck disable=SC2034
