5c5b4eef81c887a64c415f3b92c13639a250ddb2
Étienne Barrié <etienne.barrie@gmail.com>
2026-04-19 18:58:26 +0200

    demo: save & restore all prompt variables

--- .local/share/dotfiles/demo.bash
+++ .local/share/dotfiles/demo.bash
@@ -1,9 +1,11 @@
 demo() {
 	if [[ ! -v demo_save ]]; then
-		demo_save=$(declare -p PS1 PS2 PROMPT_COMMAND | sed -e 's/^declare -. //')
-		PS1="\$ " PS2="" PROMPT_COMMAND=()
+		demo_save="unset PS0 PS1 PS2 PS3 PS4 PROMPT_COMMAND demo_save;"$(
+			declare -p   PS0 PS1 PS2 PS3 PS4 PROMPT_COMMAND 2>/dev/null |
+				sed -e 's/^declare -. //')
+		unset PS0 PS2 PS3 PROMPT_COMMAND
+		PS1="\$ " PS4="+ "
 	else
 		eval -- "$demo_save"
-		unset demo_save
 	fi
 }
